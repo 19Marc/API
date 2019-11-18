8b71907ea83495a63c78fb636bc4c26027e079c9
Marc <marc@fintory.com>
2019-11-18 10:17:36 +0100

    update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
+import { TestService } from './test/test.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TestService],
 })
 export class AppModule {}
